cfdc76dd7c9981145512e4ad75be133d1d4519db
Aibar553 <74353064+Aibar553@users.noreply.github.com>
2024-01-28 19:22:39 +0600

    Add files via upload

---
+++ AspNetCore.ApiGateway/Application/IMediator.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AspNetCore.ApiGateway
+{
+    public interface IEventSourceMediator
+    {
+        IMediator AddApi(string apiKey, params string[] baseUrls);
+
+        IHubMediator AddHub(string apiKey, Func<HubConnectionBuilder, HubConnection> connectionBuilder, string receiveKey = null);
+
+        IEventSourceMediator AddEventSource(string apiKey, Func<object> connectionBuilder, string receiveKey);
+
+        IEventSourceMediator AddRoute(string routeKey, EventSourceRouteInfo routeInfo);
+
+        GatewayEventSourceRouteInfo GetRoute(string routeKey);
+
+        IEnumerable<EventSourceRoute> Routes { get; }
+
+        Dictionary<string, GatewayEventSourceRouteInfo> Paths { get; }
+    }
+
+    public interface IHubMediator
+    {
+        IMediator AddApi(string apiKey, params string[] baseUrls);
+
+        IHubMediator AddHub(string apiKey, Func<HubConnectionBuilder, HubConnection> connectionBuilder, string receiveKey = null);
+
+        IEventSourceMediator AddEventSource(string apiKey, Func<object> connectionBuilder, string routeKey);
+
+        IHubMediator AddRoute(string routeKey, HubRouteInfo routeInfo);
+
+        GatewayHubRouteInfo GetRoute(string routeKey);
+
+        IEnumerable<HubRoute> Routes { get; }
+
+        Dictionary<string, GatewayHubRouteInfo> Paths { get; }
+    }
+
+    public interface IMediator
+    {
+        IMediator AddRoute(string routeKey, GatewayVerb verb, RouteInfo routeInfo);
+
+        IMediator AddRoute(string routeKey, GatewayVerb verb, Func<ApiInfo, HttpRequest, Task<object>> exec);
+
+        GatewayRouteInfo GetRoute(string routeKey);
+
+        IMediator AddApi(string apiKey, params string[] baseUrls);
+
+        IHubMediator AddHub(string apiKey, Func<HubConnectionBuilder, HubConnection> connectionBuilder, string receiveKey = null);
+
+        IEventSourceMediator AddEventSource(string apiKey, Func<object> connectionBuilder, string receiveKey);
+
+        IEnumerable<Route> Routes { get; }
+    }
+}
